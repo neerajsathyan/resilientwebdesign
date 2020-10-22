e6f865ead5bf6e1f9b9214d5fe8528d8df5f048b
Neeraj <neeraj.sathian@gmail.com>
2020-10-22 05:06:06 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@
 
 if ('serviceWorker' in navigator) {
     // If service workers are supported
-    navigator.serviceWorker.register('/serviceworker.js');
+    navigator.serviceWorker.register('/resilientwebdesign/serviceworker.js');
 } else if ('applicationCache' in window) {
     // Otherwise inject an iframe to use appcache
     var iframe = document.createElement('iframe');
-    iframe.setAttribute('src', '/appcache.html');
+    iframe.setAttribute('src', '/resilientwebdesign/appcache.html');
     iframe.setAttribute('style', 'width: 0; height: 0; border: 0');
     document.querySelector('footer').appendChild(iframe);
 }
